afd94526d8edf9ba161ad074b0943e6c7ecf9345
Pran <83806044+PranSomboon@users.noreply.github.com>
2022-12-04 20:37:28 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
+function Dace () {
+    while (true) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+        if (makerbit.isUltrasonicDistanceLessThan(10, DistanceUnit.CM)) {
+            break;
+        }
+    }
+    Tinybit.CarCtrl(Tinybit.CarState.Car_SpinLeft)
+    basic.pause(500)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+    basic.pause(5000)
+}
+input.onGesture(Gesture.Shake, function () {
+    Dace()
+})
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P16, DigitalPin.P15)
+let strip = neopixel.create(DigitalPin.P12, 1, NeoPixelMode.RGB)
